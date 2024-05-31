ef0853b8e1f70f82a5f317ec0810008b2633149b
William <williamjoel273@gmail.com>
2024-05-31 18:31:28 -0500

    apis
    Coffee list was create with a api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { CoffeeList } from './Component/CoffeeList';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+  return (
+    <div>
+      <CoffeeList />
+    </div>
+  );
+}
+export default App;
